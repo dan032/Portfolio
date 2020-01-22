a75f578295cb7bbe1f408a596a28effd69088ccd
Daniel Butts <buttsda@shernet.sheridancollege.ca>
2020-01-22 11:08:29 -0500

    Begun process of making site responsive.

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ const Formatter = (function () {
         $main_menu.load("header.html", managePages);
     };
 
-    // Manages which link in the header is currently active, and begins relevant API requests
+    // Manages dynamic page interactivity
     const managePages = () => {
         if(window.location.pathname === "/site/index.html"){
             toggleHeader("home-link");
@@ -19,12 +19,14 @@ const Formatter = (function () {
         }
     };
 
+    // Toggles which header button is active, and alters the URL to prevent the user from loading the page again
     const toggleHeader = (id) => {
         let link = document.getElementById(id);
         link.classList.toggle("active");
         link.setAttribute("href", "#");
     };
 
+    // Performs API request to GitHub to access my current projects
     const gitHubRequest = () => {
         const url = "https://api.github.com/users/dan032/repos";
         let xhr = new XMLHttpRequest();
@@ -45,18 +47,19 @@ const Formatter = (function () {
         xhr.send();
     };
 
+    // Parses and renders the GitHub Projects
     const renderResponse = (response) =>{
         let $projects = $("#projects");
         let tmp = "";
-        const imageArr = Array("'./img/car.png'", "'./img/web.png'");
+        const imageArr = Array('./img/data.jpg', "'./img/car.png'", "'./img/web.png'");
         for (let i = 0; i < response.length; i++){
             tmp += "<div class='project'>";
-            tmp += "<img class='project-image' src=" + imageArr[i] + "/>";
-            tmp += "<p class='project-title'>" + response[i].name + "</p>";
+            tmp += `<img class='project-image' src= ${imageArr[i]}/>`;
+            tmp += `<p class='project-title'> ${response[i].name} </p>`;
             tmp += "<div class='project-link'>";
-            tmp += "<a href=" + response[i].svn_url + "><img class='mini-icon' src='./img/github.png' alt='Github Image'/></a>";
+            tmp += `<a href= ${response[i].svn_url}><img class='mini-icon' src='./img/github.png' alt='Github Image'/></a>`;
             tmp += "</div>";
-            tmp += "<p class='project-desc'>" + response[i].description + "</p>";
+            tmp += `<p class='project-desc'> ${response[i].description}</p>`;
             tmp += "</div>";
         }
         $projects.append(tmp);
