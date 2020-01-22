26a4eb0707fce7e1708a9108c38d30a9dd54ebaf
Daniel Butts <buttsda@shernet.sheridancollege.ca>
2020-01-21 22:11:44 -0500

    Implemented API call to github's API.

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,58 @@
 const Formatter = (function () {
     const $main_menu = $("#main-menu")
 
+
     const load = () => {
         $main_menu.load("header.html", manageHeader)
+
     };
 
     const manageHeader = () => {
         if(window.location.pathname === "/site/index.html"){
-            toggleHeader("home-link")
+            toggleHeader("home-link");
         }
         else if(window.location.pathname === "/site/projects.html"){
-            toggleHeader("projects-link")
+            gitHubRequest()
+            toggleHeader("projects-link");
         }
         else if (window.location.pathname === "/site/resume.html"){
-            toggleHeader("resume-link")
+            toggleHeader("resume-link");
         }
     };
 
     const toggleHeader = (id) => {
-        let link = document.getElementById(id)
-        link.classList.toggle("active")
-        link.setAttribute("href", "#")
+        let link = document.getElementById(id);
+        link.classList.toggle("active");
+        link.setAttribute("href", "#");
     };
+
+    const gitHubRequest = () => {
+        const url = "https://api.github.com/users/dan032/repos";
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.send();
+
+        xhr.onload = () => {
+            if (xhr.status != 200){
+                console.log("Error Loading Github Information")
+            }
+            else{
+                const response = JSON.parse(xhr.responseText);
+                renderResponse(response);
+            }
+        }
+
+    }
+
+    const renderResponse = (response) =>{
+        let $projects = $("#projects")
+        let tmp = "";
+
+        for (let i = 0; i < response.length; i++){
+            tmp += "<div class='project'><p>" + response[i].name + "</p>" + "<p>" + response[i].description + "</p></div>"
+        }
+        $projects.append(tmp)
+    }
 
     return {
         load,
