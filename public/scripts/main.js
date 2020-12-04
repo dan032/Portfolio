5e33b5cf97f79307ec12e75c4c5a1aeb1031dc54
dan032 <buttsda@sheridancollege.ca>
2020-12-04 04:07:29 -0500

    Updated Projects page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -43,13 +43,21 @@ const Renderer = (function () {
             if (xhr.readyState === 4 && xhr.status === 200){
                 const projects = JSON.parse(xhr.responseText);
                 const imageArr = ["../img/star.png","../img/election.png","../img/school.png", "../img/data2.png", "../img/car2.png", "../img/web2.png"];
+                const technologies = [
+                    ["C++"],
+                    ["Python", "Numpy", "Pandas"],
+                    ["React Native", "Firebase"],
+                    ["Python"],
+                    ["Java", "Room Persistence"],
+                    ["HTML", "CSS", "Javascript", "NodeJS"]
+                ];
 
                 for (let i = 0; i < projects.length; i++) {
                     tmp += "<div class='project'>";
                     tmp += "<div class='project-top'>";
                     tmp += `<img class='project-image' src=${imageArr[i]} alt="Project Image">`;
                     tmp += "</div>";
-                    tmp += "<div class='project-bottom'>";
+                    tmp += "<div class='project-middle'>";
                     tmp += `<p class="project-title">${projects[i].name}</p>`;
                     tmp += "<div class='project-link'>";
                     tmp += `<a href = ${projects[i].html_url}><img class='icons' src='img/github.png' alt='Github Image'/></a>`;
@@ -59,9 +67,13 @@ const Renderer = (function () {
                     tmp += "</div>";
                     tmp += "<p class ='project-desc'>" + projects[i].description + "</p>";
                     tmp += "</div>";
+                    tmp += "<div class='project-bottom'>";
+                    tmp += "<p style='margin: 3px'> Technologies: </p>";
+                    for (let j = 0; j < technologies[i].length; j++){
+                        tmp += `<span class="tech">${technologies[i][j]}</span>`
+                    }
                     tmp += "</div>";
-
-
+                    tmp += "</div>";
 
                 }
             }
